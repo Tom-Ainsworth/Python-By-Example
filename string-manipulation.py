ee084a9c5234024b7f4f766d74ecfd5c8b43ec3a
Tom Ainsworth <tomainsworth94@gmail.com>
2023-03-04 13:12:47 +0000

    Add string manipulation challenges

---
+++ string-manipulation.py
@@ -0,0 +1,38 @@
+# * 080
+# * Ask the user to enter their first name and then display the length of their first name.
+# * Then ask for their surname and display the length of their surname. Join their first name and surname together with a space between and display the result. Finally, display the length of their full name (including the space).
+
+
+# * 081
+# * Ask the user to type in their favourite school subject.
+# * Display it with "" after each letter, e.g. S-p-a-n-i-s-h-,
+
+
+# * 082
+# * Show the user a line of text from your favourite poem and ask for a starting and ending point. Display the characters between those two points.
+
+
+# * 083
+# * Ask the user to type in a word in upper case. If they type it in lower case, ask them to try again. Keep repeating this until they type in a message all in uppercase.
+
+
+# * 084
+# * Ask the user to type in their postcode. Display the first two letters in uppercase.
+
+
+# * 085
+# * Ask the user to type in their name and then tell them how many vowels are in their name.
+
+
+# * 086
+# * Ask the user to enter a new password. Ask them to enter it again. If the two passwords match, display "Thank you". If the letters are correct but in the wrong case, display the message "They must be in the same case" otherwise display the message "Incorrect".
+
+
+# * 087
+# * Ask the user to type in a word and then display it backwards on separate lines. For instance, if they type in "Hello" it should display as shown below:
+# * Enter a word: Hello
+# * 1
+# * 1
+# * e
+# * H
+# * >>>
