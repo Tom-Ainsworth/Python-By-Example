d7b740593945b148e53366fe60da97523188b6f8
Tom Ainsworth <tomainsworth94@gmail.com>
2023-03-06 21:48:00 +0000

    Add read/write to csv file challenges

---
+++ reading-writing-to-csv-files.py
@@ -0,0 +1,26 @@
+import csv
+
+# * 111
+# * Create a csv file that will store the data in the 'Challenge_111.jpg file. Call it "Books.cv"
+
+
+# * 112
+# * Using the Books.csv file from program 111, ask the user to enter another record and add it to the end of the file. Display each row of the .csv file on a separate line.
+
+
+# * 113
+# * Using the Books.csv file, ask the user how many records they want to add to the list and then allow them to add that many. After all the data has been added, ask for an author and display all the books in the list by that author.
+# * If there are no books by that author in the list, display a suitable message.
+
+# * 114
+# * Using the Books.cs file, ask the user to enter a starting year and an end year. Display all books released between those two years.
+
+# * 115
+# * Using the Books.csv file, display the data in the file along with the row number of each.
+
+# * 116
+# * Import the data from the Books.cv file into a list. Display the list to the user. Ask them to select which row from the list they want to delete and remove it from the list. Ask the user which data they want to change and allow them to change it.
+# * Write the data back to the original cv file, overwriting the existing data with the amended data.
+
+# * 117
+# * Create a simple maths quiz that will ask the user for their name and then generate two random questions. Store their name, the questions they were asked, their answers and their final score in a cv file. Whenever the program is run it should add to the .csv file and not overwrite anything.
